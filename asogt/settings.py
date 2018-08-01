8246cbac61b476ddb0594169fd1f9a66d46e6f61
Sabesan <sabeshuom@gmail.com>
2018-08-01 19:21:55 +1000

    static_root added

--- asogt/settings.py
+++ asogt/settings.py
@@ -18,10 +18,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #STATIC_ROOT = '/var/www/deep.tmv.io/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'proj_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'proj_static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Quick-start development settings - unsuitable for production
@@ -149,9 +152,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
