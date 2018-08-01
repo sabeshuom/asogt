7ba860954b55a1f76afa3eb743f6a288a8d7a16a
Sabesan <sabeshuom@gmail.com>
2018-08-01 19:40:22 +1000

    static_root added

--- asogt/settings.py
+++ asogt/settings.py
@@ -23,8 +23,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Quick-start development settings - unsuitable for production
